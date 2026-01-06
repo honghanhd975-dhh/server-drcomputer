2a720d1a2a612172d1f4e0d4ac764739a49c4396
ngochuyen111 <nguyenthingochuyen11.1cbq@gmail.com>
2026-01-06 15:02:43 +0700

    Initial commit

---
+++ src/main/java/com/thiep/thiep/ThiepApplication.java
@@ -0,0 +1,13 @@
+package com.thiep.thiep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThiepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThiepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thiep/thiep/ThiepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiep.thiep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThiepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
